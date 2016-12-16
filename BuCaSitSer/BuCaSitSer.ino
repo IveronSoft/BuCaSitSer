7b294d658d3de2aaca94d88211e133ff223d2e14
codiac <cdiaccc@gmail.com>
2016-12-16 07:55:30 +0200

    Add project files.

---
+++ BuCaSitSer/BuCaSitSer.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BuCaSitSer.ino
+ Created:	12/16/2016 7:55:27 AM
+ Author:	diaconuc02
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
